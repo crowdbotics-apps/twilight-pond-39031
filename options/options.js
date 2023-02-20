ebfb951d064ff7c1dab8eb9be34ef6ab0c35354e
Crowdbotics <team@crowdbotics.com>
2023-02-20 10:17:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-pond-39031",
+  url: "https://twilight-pond-39031.botics.co",
+  api: "https://twilight-pond-39031.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
